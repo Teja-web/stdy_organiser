0f5171651573544fbfff682f16dc43fbe3374915
Saiteja Jagini <154364621+Teja-web@users.noreply.github.com>
2025-11-30 19:09:38 +0530

    Add Hello.java with greeting messages

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package hello;
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello from Pipeline CI!");
+    System.out.println("Hi iam saiteja");
+    System.out.println("Hello iam from cd pipeline");
+    System.out.println("Hi iam saiteja");
+    System.out.println("Hello iam from cd pipeline");
+    System.out.println("jhj");
+    System.out.println("hdfs");
+    
+  }
+}
